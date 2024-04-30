d7e27c0fd5b29e6b08e17cea55387245e3885e55
Subham Karmakar <44800073+SubhamK108@users.noreply.github.com>
2024-04-30 23:34:20 +0530

    Refactored the error handling & feedback mechanism

--- Components/Api/WordDefinitionFetcher.cs
+++ Components/Api/WordDefinitionFetcher.cs
@@ -2,20 +2,36 @@ namespace BlazorPocketDictionary.Components.Api;
 
 public static class WordDefinitionFetcher
 {
-    public static async Task<WordDefinition> FetchWordDefinitionAsync(string word)
+    public static async Task<(WordDefinition, ResponseResult)> FetchWordDefinitionAsync(string word)
     {
         await Task.Delay(1000);
         try
         {
             using HttpClient httpClient = new();
-            var response = await httpClient.GetAsync(@$"https://api.dictionaryapi.dev/api/v2/entries/en/{word}");
+            var response = await httpClient.GetAsync(@$"https://api.dictionaryapi.dev/api/v2/entries/en/{word.Trim().ToLower()}");
             response.EnsureSuccessStatusCode();
             List<WordDefinition>? wordDefinitions = await response.Content.ReadFromJsonAsync<List<WordDefinition>>();
-            return wordDefinitions is not null ? wordDefinitions[0] : throw new Exception();
+            return (wordDefinitions is not null) switch
+            {
+                true => (wordDefinitions[0], ResponseResult.SUCCESS),
+                false => throw new Exception(nameof(ResponseResult.NOT_FOUND))
+            };
         }
-        catch (Exception)
+        catch (Exception exception)
         {
-            return new();
+            return exception.Message.Contains("404") switch
+            {
+                true => (new(), ResponseResult.NOT_FOUND),
+                false => (new(), ResponseResult.SERVER_ERROR)
+            };
         }
+    }
+
+    public enum ResponseResult
+    {
+        SUCCESS,
+        NOT_FOUND,
+        SERVER_ERROR,
+        BAD_DATA
     }
 }
